99b005c389bbf5f2b107e5a0aee902a83fd84381
Eduardo Rojas <edurojas.developer@gmail.com>
2023-06-04 22:06:24 -0300

    feat: criacao do schema.js para inserção dos dados da pagina

---
+++ js/schema.js
@@ -0,0 +1,11 @@
+const dados = {
+  title: "ESTÁ DISPONÍVEL AGORA!",
+  name: "LOKI",
+  sinopse: `Em “Loki” da Marvel Studios, o vilão mercurial Loki 
+  (Tom Hiddleston) retoma seu papel como o Deus do Mal em uma nova 
+  série que ocorre após os eventos de “Vingadores: Endgame”. `,
+  tempo: "51min",
+  ano: 2021,
+  btnTrailer: "TRAILER",
+  btnSerie: "ASSISTIR AGORA"
+};
